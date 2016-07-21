1af70cfd068b66f596d39254b8dbfaa5408813d7
cwchiu <sisimi.pchome@gmail.com>
2016-07-21 11:33:16 +0800

    mysql test

---
+++ example/002_Lib/mysql-test/test.js
@@ -0,0 +1,21 @@
+let mysql = require('mysql-co'),
+    co = require('co');
+
+function test_mysql(){
+    co(function *() {
+      var db = mysql.createConnection({host:'127.0.0.1' , user: 'root', database: 'test', password: ''});
+      console.log( yield db.query("select 1+1 as qqq") );
+      console.log( yield db.query("select 1+2 as qqq") );
+      console.log( yield db.query("select 1+3 as qqq") );
+      db.end();
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+
+function main(){
+    test_mysql();
+}
+
+main();
