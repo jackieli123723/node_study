6c8f33ffa8130dc763d3d04952fa598ab4e1296e
cwchiu <sisimi.pchome@gmail.com>
2016-08-05 00:15:52 +0800

    class example update

---
+++ example/001_Syntax/Classes/README.md
@@ -0,0 +1,10 @@
+* [javascript ES 6 class 詳解 - 官文祥 - 博客園](http://www.cnblogs.com/E-WALKER/p/4796278.html)
+* [Object-Oriented JavaScript — A Deep Dive into ES6 Classes](https://www.sitepoint.com/object-oriented-javascript-deep-dive-es6-classes/)
+* [ES6中的Class](http://www.cnblogs.com/linda586586/p/5102670.html)
+* [Managing the private data of ES6 classes](http://www.2ality.com/2016/01/private-data-classes.html)
+* [ES6 Classes in Depth](https://ponyfoo.com/articles/es6-classes-in-depth?utm_source=javascriptweekly&utm_medium=email)
+* [ES6 classes have inner names](http://www.2ality.com/2016/01/classes-inner-names.html)
+* extends後面可以接駁任意合法且擁有prototype屬性的構造函數
+* [深入淺出ES6（十五）：子類 Subclassing](http://www.linuxeden.com/html/news/20151119/163839.html)
+* [javascript ES 6 class 詳解 - 官文祥 - 博客園](http://www.cnblogs.com/E-WALKER/p/4796278.html)
+
--- example/001_Syntax/Classes/test.js
+++ example/001_Syntax/Classes/test.js
@@ -1,21 +1,63 @@
 
 function es6_classes(){
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+    
+    console.log('== es6 class ==');
     class Person {
+        // 建構子
         constructor(name) {
             this.name = name;
         }
+        
+        // instance method
         describe() {
             return 'Person called ' + this.name;
         }
+        
+        *random(){
+            while(true){
+                yield Math.random();
+            }
+        }
+        
+        // static property get
+        static get Style() {
+            console.log('>> static property get');
+            return Person.style;
+        }
+        
+        // static property set
+        static set Style(val) {
+            console.log('>> static property set');
+            Person.style = val;
+        }
+        
+        // dynamic method
+        ['dynamic' + '_' + 'method']() {
+            console.log('dynamic method');
+        }
     }
         
+
+    console.log(`typeof Person = ${typeof Person}`);
+    let p = new Person('arick');
+    console.log(p.describe());
+    console.log(p.hasOwnProperty('name'));
+    Person.Style = "Superman";
+    console.log(Person.Style);
+    p.dynamic_method();
+    let rnd = p.random();
+    console.log(rnd.next());
+    console.log(rnd.next());
+    console.log('== subclass ==');
+    
     // Subclass
     class Employee extends Person {
         constructor(name, title) {
             super(name); 
             this.title = title;
         }
+        
         describe() {
             return super.describe() + ' (' + this.title + ')';
         }
@@ -25,31 +67,37 @@ function es6_classes(){
         
         }
         
-        // property
+        // instance property get
         get name(){
           return "arick";
         }
         
+        // instance property set
         set name(v){
           console.log('set name:'+v)
         }
     }
-
+    
+    let p2 = new Employee('arick', 'CEO');
+    console.log(p2.describe());
+    
+    console.log(`p2 instanceof Employee = ${p2 instanceof Employee}`);
+    console.log(`p2 instanceof Person = ${p2 instanceof Person}`);
+    console.log(`Object.getPrototypeOf(p2) = ${Person == Object.getPrototypeOf(p2)}`);
+    
+    console.log('== extends Error ==');
     class MyError extends Error {
 
     }
-    var p = new Person('arick');
-    console.log(p.describe());
-
-    var p2 = new Employee('arick', 'CEO');
-    console.log(p2.describe());
+    
     try{
         throw new MyError();
     }catch(ex){
+        console.log(ex instanceof MyError);
         console.log('catch myerror');
     }
 
-
+    console.log('== Iterator ==');
     // Class的Generator方法
     class Foo {
         constructor(...args) {        this.args = args;
@@ -86,6 +134,15 @@ function es4_class(){
 function main(){
     es6_classes();
     es4_class();
+    
+    // Object.is該方法可以用來判斷兩個值是否一樣，可以判斷NaN與NaN，-0和+0，採用的是等值算法，如下所示
+    console.log(`Object.is(NaN,NaN)=`+Object.is(NaN,NaN));
+    console.log(`Object.is(+0,-0)=`+Object.is(+0,-0));
+    console.log(`Object.is({x:1},{x:1})=`+Object.is({x:1},{x:1}));
+
+    //Object.assign用來合併對象，採用的是淺複製且存在同名屬性後者會覆蓋前者，如下所示
+    console.log(`Object.assign({x:1}, {y:2}, {y:3,x:2})=`
+        + JSON.stringify(Object.assign({x:1}, {y:2}, {y:3,x:2})));
 }
     
 main();
