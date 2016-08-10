c2ebafa8fe44348057c79f00c5efd551778bcd4a
cwchiu <sisimi.pchome@gmail.com>
2016-08-10 21:26:54 +0800

    add code comment

--- example/003_DB/mongo-test/test.js
+++ example/003_DB/mongo-test/test.js
@@ -1,19 +1,26 @@
-let comongo = require('co-mongodb')
+const comongo = require('co-mongodb')
     , co = require('co');
 
 
 function main(){
     
     co(function *() {
+      // 連線
       let db = yield comongo.client.connect('mongodb://localhost:27017/test');
       
+      // 取得 collection
       let col = yield comongo.db.collection(db, 'testCollection');
       
+      // 新增
       yield col.insert({'name': 'arick'});
-      
+      yield col.update({'name': 'arick'}, {'$set':{
+          level: 99
+      }});
+      // 搜尋
       let res = yield col.findOne({'name': 'arick'});
       console.log(res);
       
+      // 刪除
       yield col.remove({'name': 'arick'});
       
       yield comongo.db.close(db);
