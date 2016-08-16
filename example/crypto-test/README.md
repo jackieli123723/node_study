4776c98dd7d39ad00a117900e1ead0e71876d6db
cwchiu <sisimi.pchome@gmail.com>
2016-08-16 22:22:37 +0800

    crypto example update

---
+++ example/crypto-test/README.md
@@ -0,0 +1,2 @@
+* [Example of using crypto.pbkdf2 to hash and verify passwords asynchronously using bluebird promise, while storing the hash and salt in a single combined buffer along with the original hash settings](https://gist.github.com/mba7/979e6c3fe715fc618549fae4d09019ef)
+* [Node.js `crypto.final` make the encrypted result is different to PHP `mcrypt_encrypt` - Stack Overflow](http://stackoverflow.com/questions/30024531/node-js-crypto-final-make-the-encrypted-result-is-different-to-php-mcrypt-enc)
--- example/crypto-test/test.js
+++ example/crypto-test/test.js
@@ -1,4 +1,5 @@
-const crypto = require('crypto');
+const crypto = require('crypto')
+      fs = require('fs');
 
 
 function sha256(){
@@ -9,7 +10,70 @@ function sha256(){
     console.log(hash);
 }
 
+function hmac_test(){
+    const hashs = crypto.getHashes();
+    const msg = 'hello crypto.HMAC';
+    const key = '0123456789123456';
+    for(let hash_name of hashs){
+        console.log(`== HMAC.${hash_name} ==`);
+        console.log(crypto.createHmac(hash_name, key)
+                       .update(msg)
+                       .digest('hex'));
+    }
+}
+
+function hash_test(){
+    const hashs = crypto.getHashes();
+    const msg = 'hello crypto.hash';
+    for(let hash_name of hashs){
+        console.log(`== Hash.${hash_name} ==`);
+        console.log(crypto.createHash(hash_name)
+                       .update(msg)
+                       .digest('hex'));
+    }
+}
+
+function cipher_test(){
+    // const algos = crypto.getCiphers();
+    const msg = 'hello crypto.cipher';
+    const key = '1471341432865';
+    let algo = 'aes192';
+    console.log(`== Cipher.${algo} ==`);
+    let cipher  = crypto.createCipher(algo, key)
+    let encrypted = cipher.update(msg, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    console.log(encrypted);
+    
+    const decipher = crypto.createDecipher(algo, key);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    console.log(decrypted);
+}
+
+function file_md5(){
+    let md5 = crypto.createHash('md5');
+    fs.createReadStream('test.js')
+       .on('data', function(v){
+            md5.update(v);
+       }).on('end', function(){
+            console.log(md5.digest('hex'));
+       });
+}
+
+function random_string(){
+    let buf = crypto.randomBytes(16);
+    console.log('== random string ==');
+    console.log(buf.toString('hex'));
+    console.log(buf.toString('base64'));
+}
+
 function main(){
-    sha256();
+    // sha256();
+    // hash_test();
+    // hmac_test();
+    // cipher_test();
+    // random_string();
+    // file_md5();
+    
 }
 main();
