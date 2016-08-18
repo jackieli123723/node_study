ea0a6fa69eb41d0267faf7dceab0979103281f2a
cwchiu <sisimi.pchome@gmail.com>
2016-08-18 14:31:43 +0800

    redis pub/sub example

--- example/003_DB/Redis/test.js
+++ example/003_DB/Redis/test.js
@@ -35,6 +35,33 @@ class DB{
     }
 }
 
+function pubsub(){
+    
+    let master = redis.createClient();
+    let worker1 = redis.createClient();
+    worker1.on('subscribe', (channel, count)=>{
+        console.log(`Worker#1 .subscribe >> Recv ${channel}, ${count}`);
+    });
+    worker1.on('message', (channel, message)=>{
+        console.log(`Worker#1 .message>> Recv ${channel}, ${message}`)
+    });
+    worker1.subscribe('chat');
+    worker1.subscribe('chat');
+    
+    let worker2 = redis.createClient();
+    worker2.on('subscribe', (channel, count)=>{
+        console.log(`Worker#2 .subscribe >> Recv ${channel}, ${count}`);
+    });
+    worker2.on('message', (channel, message)=>{
+        console.log(`Worker#2 .message>> Recv ${channel}, ${message}`)
+    }); 
+    worker2.subscribe('chat');
+    worker2.subscribe('game');
+    setInterval( ()=>{
+        master.publish('chat', Number(new Date()));
+        master.publish('game', Math.random());
+    }, 1000);
+}
 
 function main(){
     let db = new DB();
@@ -47,6 +74,9 @@ function main(){
             return db.close();
         })
         .catch( err => console.error(err) );
+        
+    
+    pubsub();
 }
 module.exports.DB = DB;
 
