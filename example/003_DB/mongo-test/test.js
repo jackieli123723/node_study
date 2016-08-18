a57cab6ae7c5ae0a722c3f69170e8ab50bf6c58d
cwchiu <sisimi.pchome@gmail.com>
2016-08-18 15:08:09 +0800

    mongo client example update

--- example/003_DB/mongo-test/test.js
+++ example/003_DB/mongo-test/test.js
@@ -1,8 +1,94 @@
 const comongo = require('co-mongodb')
-    , co = require('co');
+    , co = require('co')
+    ,MongoClient = require('mongodb').MongoClient;
 
+class DB{
+    static factory(url){
+        return new Promise( (y,n)=>{
+            MongoClient.connect(url, (err, client) =>{
+                if(err){
+                    n(err);
+                }else{
+                    y( new DB(client) );
+                }
+            });
+        });
+    }
+    
+    constructor(client){
+        this.client = client;
+    }
+    
+    update(col_name, where, new_doc){
+        let col = this.client.collection(col_name);
+        return new Promise((y,n)=>{
+            col.updateOne(where, new_doc, (err, result)=>{
+                if(err){
+                    n(err);
+                }else{
+                    y(result);
+                }
+            });
+        });
+    }
+    
+    insertMany(col_name, doc){
+        let col = this.client.collection(col_name);
+        return new Promise((y,n)=>{
+            col.insertMany([ doc ], (err, result)=>{
+                if(err){
+                    n(err);
+                }else{
+                    y(result);
+                }
+            });
+        });
+    }
+    
+    deleteMany(col_name, where){
+        let col = this.client.collection(col_name);
+        return new Promise((y,n)=>{
+            col.deleteMany(where, (err, result)=>{
+                if(err){
+                    n(err);
+                }else{
+                    y(result);
+                }
+            });
+        });
+    }
+    
+    findMany(col_name, where){
+        let col = this.client.collection(col_name);
+        return new Promise((y,n)=>{
+            col.find(where, (err, result)=>{
+                if(err){
+                    n(err);
+                }else{
+                    y(result);
+                }
+            });
+        });
+    }
+    findOne(col_name, where){
+        let col = this.client.collection(col_name);
+        return new Promise((y,n)=>{
+            col.findOne(where, (err, result)=>{
+                if(err){
+                    n(err);
+                }else{
+                    y(result);
+                }
+            });
+        });
+    }
+    
+    close(){
+        this.client.close();
+    }
+}
 
-function main(){
+function test_with_co(){
     
     co(function *() {
       // ³s½u
@@ -25,6 +111,38 @@ function main(){
       
       yield comongo.db.close(db);
     });
+}
+
+function test_with_promise(){
+    
+    DB.factory('mongodb://localhost:27017/test')
+        .then( (client) => {
+            return client.insertMany('game', {'name': 'arick'})
+                .then( v=> {
+                    console.log(`insert : ${JSON.stringify(v)}`);
+                    return client.update('game', {'name': 'arick'}, {'$set':{
+                        level: 99
+                    }});
+                })
+                .then( (v) => {
+                    console.log(`update : ${v}`);
+                    return client.findOne('game', {'name': 'arick'});
+                })
+                .then( (v) => {
+                    console.log(`find : ${JSON.stringify(v)}`);
+                    return client.deleteMany('game', {'name': 'arick'});
+                })
+                .then( (v) => {
+                    console.log(`delete : ${v}`);
+                    return client.close(); 
+                })
+                .catch( err => console.error(err) );
+        });
+}
+
+function main(){
+    test_with_co();
+    test_with_promise();
 }
     
 main();
