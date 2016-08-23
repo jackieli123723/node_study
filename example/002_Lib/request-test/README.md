0798b47c27a30458d681d46a34e4283a21198293
cwchiu <sisimi.pchome@gmail.com>
2016-08-23 17:11:01 +0800

    request example

---
+++ example/002_Lib/request-test/README.md
@@ -0,0 +1,6 @@
+
+# 檔案下載
+* [request - Node.js Download File Using Content Disposition as Filename - Stack Overflow](http://stackoverflow.com/questions/20132064/node-js-download-file-using-content-disposition-as-filename)
+
+# 參考資料
+* [request/request: Simplified HTTP request client.](https://github.com/request/request)
---
+++ example/002_Lib/request-test/test.js
@@ -0,0 +1,101 @@
+let request = require('request'),
+    fs = require('fs');
+
+// 忽略 HTTPS 證書檢查
+// http://stackoverflow.com/questions/19665863/how-do-i-use-a-self-signed-certificate-for-a-https-node-js-server
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+function httpGet(){
+    request({
+        'method' : 'get',
+        'url': 'https://api.ipify.org?format=json',
+        'headers': {
+            'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)'
+        }
+    }, (err, resp) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        let jd = JSON.parse(resp.body);
+        console.log(jd.ip);
+    });
+}
+
+function formPost(){
+    let jar = request.jar();
+    request({
+        'method' : 'post',
+        'url': 'http://127.0.0.1:3000/rest/login',
+        'headers': {
+            'User-Agent': 'xxxApp/Windows'
+        },
+        'form':{
+            'UserID':'test',
+            'Passwd':'123'
+        },
+        'jar' : jar
+    }, (err, resp) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log(resp.body);
+        console.log(jar);
+    });
+}
+
+function jsonPost(){
+    request({
+        'method' : 'post',
+        'url': 'http://127.0.0.1:3000/rest/hello/world',
+        'headers': {
+            'User-Agent': 'xxxApp/Windows'
+        },
+        'body': {'msg': 'secret'},
+        'json': true
+    }, (err, resp) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log(resp.body);
+    });
+}
+
+function fileDownload(){
+    
+    request.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg")
+        .on('response', (resp)=>{
+            let file = fs.createWriteStream("file.jpg");
+            resp.pipe(file);
+        });
+}
+
+function readLargeText(){
+    let readline = require('readline');
+    request.get("http://127.0.0.1/api/down/1463466389,1466069296")
+        .on('response', (resp)=>{
+            console.log('==response==');
+            if(resp.statusCode==200){
+                let total = -1;
+                let recv = 0;
+                let rl = readline.createInterface({
+                  input: resp
+                });
+                rl.on('line', (line) => {
+                  console.log(line);
+                });
+            }
+        });
+}
+
+function main(){
+    // httpGet();
+    formPost();
+    // fileDownload();
+    // readLargeText();
+    // jsonPost();
+}
+
+main();
