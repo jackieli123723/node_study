0d8610a24c0173627d26579f41606a5f457ae0a5
cwchiu <sisimi.pchome@gmail.com>
2016-09-07 16:29:12 +0800

    localmyip example update

--- example/006_Net/LocalMyIP/README.md
+++ example/006_Net/LocalMyIP/README.md
@@ -1,1 +1,2 @@
 * [javascript - Get local IP address in node.js - Stack Overflow](http://stackoverflow.com/questions/3653065/get-local-ip-address-in-node-js)
+* [How can I get the local IP address in Node.js? - Stack Overflow](http://stackoverflow.com/questions/10750303/how-can-i-get-the-local-ip-address-in-node-js)
--- example/006_Net/LocalMyIP/test.js
+++ example/006_Net/LocalMyIP/test.js
@@ -1,3 +1,52 @@
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  console.log('addr: '+add);
-});
+const
+    dns = require('dns'),
+    os = require('os');
+
+function listMyIp(){
+    let interfaces = os.networkInterfaces();
+    let addresses = {};
+    for (var k in interfaces) {
+        // console.log(k);
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses[k]= address;
+            }
+        }
+    }
+
+    return addresses;
+}
+
+function hostname2ip(hostname){
+    hostname = hostname || os.hostname();
+    
+    return new Promise( (y,n)=>{
+        dns.lookup(hostname, function (err, addr, fam) {
+            if(err){
+                n(err);
+            }else{
+                y(addr);
+            }
+        });
+    });
+}
+
+function main(){
+    Promise.all([
+        hostname2ip('localhost'),
+        hostname2ip('127.0.0.1'),
+        hostname2ip()
+    ])
+        .then( console.log )
+        .catch( console.error);
+        
+     console.log(listMyIp());
+}
+   
+module.exports.listMyIp = listMyIp;
+module.exports.hostname2ip = hostname2ip;
+
+if (require.main === module) {
+    main();
+}
