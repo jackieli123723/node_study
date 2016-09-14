4717ee8543b689c834eb9cb6a577cbcde57de1f1
cwchiu <sisimi.pchome@gmail.com>
2016-09-14 22:54:10 +0800

    string example update

---
+++ example/015_DateTime/DateTime/README.md
@@ -0,0 +1,1 @@
+* [Date - JavaScript | MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date)
--- example/015_DateTime/DateTime/test.js
+++ example/015_DateTime/DateTime/test.js
@@ -1,59 +1,84 @@
-
-/**
- * 對Date的擴展，將 Date 轉化為指定格式的String
- * 月(M)、日(d)、小時(h)、分(m)、秒(s)、季度(q) 可以用 1-2 個佔位符， 
- * 年(y)可以用 1-4 個佔位符，毫秒(S)只能用 1 個佔位符(是 1-3 位的數字) 
- * 例子： 
- * (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
- * (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
-*/
-Date.prototype.format = function (fmt) { //author: meizz 
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小時 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
-};
-
-function today_is(){
-    const day_list = ['日', '一', '二', '三', '四', '五', '六'];
-    let day = day_list[new Date().getDay()]; 
-
-    console.log(`Today is 星期${day}`);
-}
+const
+    assert = require('assert'),
+    util = require('./index');
+    
+describe('basic', function(){
+    it('new Date(dateString)', ()=>{
+        let d = new Date('2009/2/13 23:31:30Z');
+        assert.equal(Number(d), 1234567890000);
+    });
+    
+    it('es5.1 : Date.now() - 取得目前的 timestamp ', ()=>{
+        let t1 = Date.now();
+        let d2 = new Date();
+        assert.equal( t1, Number(d2));
+    });
+    
+    it('UTC', ()=>{
+        let t = Date.UTC('2009','01','13','23','31','30');
+        assert.equal( typeof(t), 'number');
+        
+        let d = new Date(t);
+        assert.equal(d.getUTCFullYear(), 2009);
+        assert.equal(d.getUTCMonth(), 1); // 0=1月, 1=2月...
+        assert.equal(d.getUTCDate(), 13);
+        assert.equal(d.getUTCHours(), 23);
+        assert.equal(d.getUTCMinutes(), 31);
+        assert.equal(d.getUTCSeconds(), 30);
+    });
+    
+    it('.getTime() 取 timestamp', ()=>{
+        let d = new Date(Date.UTC('2009','01','13','23','31','30'));
+        assert.equal(d.getTime(), 1234567890000);
+        assert.equal(Number(d), 1234567890000);
+    });
+    it('TimeStamp <-> Date', ()=>{
+        let d = new Date();
+        let t = Number(d);
+        let d2 = new Date(t);
+        
+        assert.equal(d.getMilliseconds(), d2.getMilliseconds());
+        assert.equal(Number(d2), t);
+    });
+    
+    
+    it('字串轉 Date', ()=>{
+        let utc_date = Date.parse('2016-08-04 07:04:22Z');
+        let local_date = Date.parse('2016-08-04 15:04:22'); // +8
+        let timezone_date = Date.parse('2016-08-04 15:04:22+8:00'); 
+        assert.equal(Number(utc_date), 1470294262000);
+        assert.equal(Number(timezone_date), 1470294262000);
+        assert.equal(Number(local_date), Number(timezone_date));
+    });
+    it('時間最小值', ()=>{
+        let d2 = new Date(0);
+        assert.equal(Number(d2), 0);
+    });
+    
+    it('.format()', ()=>{
+        let s = util.format(new Date(2016, 8, 4, 13, 59, 58, 3),"yyyy-MM-dd hh:mm:ss.S");
+        assert.equal(s, '2016-09-04 13:59:58.3');
+    });
+    
+    it('明確指定年月日時分秒', ()=>{
+        let d1 = new Date(2016, 8, 4, 13, 59, 58, 3);
+        
+        assert.equal(Number(d1), 1472968798003);
+        assert.equal(d1.getFullYear(), 2016);
+        assert.equal(d1.getMonth(), 8);
+        assert.equal(d1.getDate(), 4);
+        assert.equal(d1.getHours(), 13);
+        assert.equal(d1.getMinutes(), 59);
+        assert.equal(d1.getSeconds(), 58);
+        assert.equal(d1.getMilliseconds(), 3);
+    });
+    
+});
 function main(){
-    console.log(`new Date() = ${new Date()}`);
-    console.log(`new Date(0) = ${new Date(0)}`);
-    console.log(`new Date(2016, 8, 4, 13, 59, 58, 3) = ${new Date(2016, 8, 4, 13, 59, 58, 3)}`);
-
-    let d = new Date();
-    let t = new Number(d);
-    let d2 = new Date( t );
-
-    console.log(`
-    ======
-    let d = new Date();
-    let t = new Number(d);
-    let d2 = new Date( ${t} );
-    ${d2} == ${d}
-    ======
-    `);
-
     // utc to localtime
     console.log( (new Date(Date.parse('2016-08-04 07:04:22Z'))).toLocaleString()	);
-    console.log( Date.parse('2016-08-04 07:04:22Z') );
     
-    console.log((new Date()).format("yyyy-MM-dd hh:mm:ss.S"));
-    
-    today_is();
+    util.todayIs();
 }
     
 if (require.main === module) {
