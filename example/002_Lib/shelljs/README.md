e7c6b931231015f9046fa1b8d7b27eb66394ea66
cwchiu <sisimi.pchome@gmail.com>
2016-09-17 23:45:53 +0800

    shelljs

---
+++ example/002_Lib/shelljs/README.md
@@ -0,0 +1,1 @@
+* [node.js 調用 shelljs 接收 stdin 輸入輸出 一介布衣](http://yijiebuyi.com/blog/2b5fe4ecbcf640a473ee7db224d7f3ed.html)
---
+++ example/002_Lib/shelljs/test.js
@@ -0,0 +1,12 @@
+
+function main(){
+    console.log('hello world');
+}
+
+module.exports = {
+    
+};
+    
+if (require.main === module) {
+    main();
+}
