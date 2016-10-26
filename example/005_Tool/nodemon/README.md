b6213e0168ccfab74437f3d02efd0b80c90cbeba
cwchiu <sisimi.pchome@gmail.com>
2016-10-26 15:01:08 +0800

    nodemon example

---
+++ example/005_Tool/nodemon/README.md
@@ -0,0 +1,6 @@
+* nodemon test.js
+* nodemon test.js -- -v 
+    * 加參數
+* nodemon test.js --exitcrash test.js
+    * 當機離開
+    
---
+++ example/005_Tool/nodemon/test.js
@@ -0,0 +1,12 @@
+
+function main(){
+    console.log('hello world #3');
+}
+
+module.exports = {
+    
+};
+    
+if (require.main === module) {
+    main();
+}
